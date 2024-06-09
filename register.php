9877a7381cb185db373cc5b5de8c45ca93819297
RegineRubante <rm3387698@gmail.com>
2024-06-10 00:08:40 +0700

    rff

--- register.php
+++ register.php
@@ -20,3 +20,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $conn->close();
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RFF</title>
