88af723ab957fd51b891363385e9d7de644e0ef9
RegineRubante <rm3387698@gmail.com>
2024-06-10 00:05:50 +0700

    post

---
+++ register.php
@@ -0,0 +1,7 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $role = $_POST['role'];
