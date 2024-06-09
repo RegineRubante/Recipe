76d56a64134fdefcbca826fe9e1010b54be02366
RegineRubante <rm3387698@gmail.com>
2024-06-10 00:07:12 +0700

    echo

--- register.php
+++ register.php
@@ -5,3 +5,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
     $role = $_POST['role'];
+
+    $sql = "INSERT INTO users (username, password, role) VALUES ('$username', '$password', '$role')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>
+                alert('Account successfully created!'); 
+                window.location.href = 'http://localhost/MyRecipe/login.php';
+              </script>";
+        exit();
